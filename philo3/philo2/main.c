30a406d93c5a1be1f01c2a8bea13eee80eef6d97
Pinxito <lobaestepparia@gmail.com>
2025-10-30 05:42:52 +0100

    primer push

---
+++ philo3/philo2/main.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguillen <gguillen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 12:37:47 by gguillen          #+#    #+#             */
+/*   Updated: 2025/10/30 05:37:54 by gguillen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	should_print_event(t_shared *s, char code)
+{
+	int	dead;
+	int	finished;
+
+	pthread_mutex_lock(&s->state_lock);
+	dead = s->someone_dead;
+	finished = s->all_finished;
+	pthread_mutex_unlock(&s->state_lock);
+	if ((dead || finished) && code != 'D')
+		return (0);
+	return (1);
+}
+
+void	show_event(t_shared *s, t_philosopher *ph, char code)
+{
+	long	time;
+
+	if (!should_print_event(s, code))
+		return ;
+	pthread_mutex_lock(&s->print_lock);
+	if (!should_print_event(s, code))
+	{
+		pthread_mutex_unlock(&s->print_lock);
+		return ;
+	}
+	time = current_time_ms() - s->start_ms;
+	if (code == 'F')
+		printf("%ld %d has taken a fork\n", time, ph->id);
+	else if (code == 'E')
+		printf("%ld %d is eating\n", time, ph->id);
+	else if (code == 'S')
+		printf("%ld %d is sleeping\n", time, ph->id);
+	else if (code == 'T')
+		printf("%ld %d is thinking\n", time, ph->id);
+	else if (code == 'D')
+	{
+		printf("%ld %d died\n", time, ph->id);
+		set_someone_dead(s, 1);
+	}
+	pthread_mutex_unlock(&s->print_lock);
+}
+
+int	main(int argc, char **argv)
+{
+	t_shared	data;
+
+	if (argc != 5 && argc != 6)
+		return (printf(MSG_USAGE), 1);
+	if (init_shared(&data, argv))
+		return (1);
+	if (init_philos(&data))
+		return (1);
+	if (start_simulation(&data))
+		return (1);
+	return (0);
+}
---
+++ philo3/philo2/simulation.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simulation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguillen <gguillen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 13:21:36 by gguillen          #+#    #+#             */
+/*   Updated: 2025/10/30 05:31:57 by gguillen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	cleanup_all(t_shared *s)
+{
+	int	i;
+
+	i = -1;
+	while (++i < s->philos_count)
+	{
+		pthread_mutex_destroy(&s->forks[i]);
+	}
+	pthread_mutex_destroy(&s->print_lock);
+	pthread_mutex_destroy(&s->state_lock);
+	pthread_mutex_destroy(&s->meal_lock);
+	free(s->forks);
+	free(s->list);
+	return (0);
+}
+
+static int	create_philosophers(t_shared *s)
+{
+	int	i;
+	int	created;
+
+	created = 0;
+	i = -1;
+	while (++i < s->philos_count)
+	{
+		if (pthread_create(&s->list[i].thread, NULL, philo_routine,
+				&s->list[i]) != 0)
+		{
+			printf(MSG_THREAD);
+			set_someone_dead(s, 1);
+			while (created > 0)
+			{
+				created--;
+				pthread_join(s->list[created].thread, NULL);
+			}
+			cleanup_all(s);
+			return (1);
+		}
+		created++;
+	}
+	return (0);
+}
+
+int	start_simulation(t_shared *s)
+{
+	int	i;
+
+	s->start_ms = current_time_ms();
+	i = -1;
+	while (++i < s->philos_count)
+		s->list[i].last_meal_ms = s->start_ms;
+	if (create_philosophers(s))
+		return (1);
+	if (s->philos_count > 1)
+		monitor_loop(s);
+	i = -1;
+	while (++i < s->philos_count)
+		pthread_join(s->list[i].thread, NULL);
+	cleanup_all(s);
+	return (0);
+}
+
+void	one_philo_case(t_shared *s, t_philosopher *ph)
+{
+	pthread_mutex_lock(&s->forks[ph->right_fork]);
+	show_event(s, ph, 'F');
+	pthread_mutex_unlock(&s->forks[ph->right_fork]);
+	smart_sleep(s->time_die, s);
+	show_event(s, ph, 'D');
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philosopher	*ph;
+	t_shared		*s;
+
+	ph = (t_philosopher *)arg;
+	s = ph->shared;
+	if (s->philos_count == 1)
+	{
+		one_philo_case(s, ph);
+		return (NULL);
+	}
+	if (ph->id % 2 == 0)
+		smart_sleep(s->time_eat / 2, s);
+	while (!is_someone_dead(s) && !is_all_finished(s))
+	{
+		perform_eat(s, ph);
+		if (is_someone_dead(s) || is_all_finished(s))
+			break ;
+		show_event(s, ph, 'S');
+		smart_sleep(s->time_sleep, s);
+		show_event(s, ph, 'T');
+		usleep(100);
+	}
+	return (NULL);
+}
