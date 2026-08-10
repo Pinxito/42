d4e07b9ff2f875346c37126f28b59272069ebd08
Maria Gabriella Guillen Lopez <gguillen@MacBook-Pro-de-Maria.local>
2026-08-11 00:31:29 +0200

    ultimo cambio

--- cpp/cpp00/ex00/megaphone.cpp
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -11,29 +11,30 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+#include <cctype>
 
 int    main(int ac, char **av)
 {
     int    i;
-    int    j;
 
     i = 1;
-    j = 0;
     if (ac < 2)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
         while (av[i])
         {
-            j = 0;
-            while (av[i][j])
-            {
-                std::cout << (char)std::toupper(av[i][j]);
-                j++;
-            }
+            
+            
+            std::string word(av[i]);
+            for (std::string::size_type j = 0; j < word.size(); j++)
+            word[j] = std::toupper(static_cast<unsigned char>(word[j]));
+            std::cout << word;
             i++;
         }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
+    
     return (0);
 }
